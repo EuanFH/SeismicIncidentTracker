5501f5d525f88f4244e77aec76ba0aa9e917b6b1
Euan Haahr <euanhahrmail2@gmail.com>
2019-03-14 22:21:24 +0000

    Search PopupMenu nearly finished.

--- app/src/main/java/net/chinzer/seismicincidenttracker/FragmentSortSearchActionBar.java
+++ app/src/main/java/net/chinzer/seismicincidenttracker/FragmentSortSearchActionBar.java
@@ -1,10 +1,22 @@
 package net.chinzer.seismicincidenttracker;
 
+import android.animation.ObjectAnimator;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.icu.util.Calendar;
+import android.text.format.Time;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.PopupMenu;
+import android.widget.PopupWindow;
+import android.widget.TimePicker;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
@@ -29,9 +41,39 @@ public abstract class FragmentSortSearchActionBar extends Fragment {
         {
 
             case R.id.search_button:
+
+                EditText startDate;
+                EditText endDate;
+                EditText startTime;
+                EditText endTime;
+                EditText startMagnitude;
+                EditText endMagnitude;
+                EditText startDepth;
+                EditText endDepth;
                 //View searchButtonView = this.getActivity().findViewById(R.id.search_button);
                 //PopupMenu searchMenu = new PopupMenu(this.getContext(), searchButtonView);
-                seismicIncidentViewModel.testSearchSeismicIncidents(currentSortColumn, currentAscending);
+                //seismicIncidentViewModel.testSearchSeismicIncidents(currentSortColumn, currentAscending);
+                LayoutInflater layoutInflater = (LayoutInflater) getActivity().getSystemService(getContext().LAYOUT_INFLATER_SERVICE);
+                View popupMenuView = layoutInflater.inflate(R.layout.popup_search,null);
+
+                PopupWindow popupWindow = new PopupWindow(popupMenuView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                startDate = popupMenuView.findViewById(R.id.startDate);
+                endDate = popupMenuView.findViewById(R.id.endDate);
+                startTime = popupMenuView.findViewById(R.id.startTime);
+                endTime = popupMenuView.findViewById(R.id.endTime);
+
+                addDateDialog(startDate);
+                addDateDialog(endDate);
+                addTimeDialog(startTime);
+                addTimeDialog(endTime);
+
+                popupWindow.setFocusable(true);
+                popupWindow.setElevation(40);
+                popupWindow.update();
+                getActivity().findViewById(R.id.fragmentContainer).setForeground(getResources().getDrawable( R.drawable.window_dim ));
+                fadeInForeground(getActivity().findViewById(R.id.fragmentContainer));
+
+                popupWindow.showAtLocation(getView(), Gravity.CENTER, 0, 0);
                 return true;
 
             case R.id.sort_button:
@@ -105,6 +147,49 @@ public abstract class FragmentSortSearchActionBar extends Fragment {
                 return super.onOptionsItemSelected(item);
 
         }
+    }
+
+    private void fadeInForeground(View view) {
+        ObjectAnimator animator = ObjectAnimator.ofInt(view.getForeground(), "alpha", 0, 120);
+        animator.setDuration(500);
+        animator.start();
+    }
+
+    private void addDateDialog(EditText dateField){
+        //way to get currently selected
+        dateField.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar c = Calendar.getInstance();
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePicker = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+                    public void onDateSet(DatePicker datepicker, int selectedyear, int selectedmonth, int selectedday) {
+                        dateField.setText(String.format("%02d/%02d/%02d", selectedday, selectedmonth, selectedyear));
+                    }
+                },year, month, day);
+                datePicker.show();
+            }
+        });
+    }
+
+    private void addTimeDialog(EditText timeField){
+        //way to get currently selected
+        timeField.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int hour = calendar.get(Calendar.HOUR_OF_DAY);
+                int minute = calendar.get(Calendar.MINUTE);
+                TimePickerDialog timePicker = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        timeField.setText(String.format("%02d:%02d", selectedHour, selectedMinute));
+                    }
+                },hour, minute, true);
+                timePicker.show();
+            }
+        });
     }
 
 }
